21fdfca9634c68732e3bea01d08bf121f45e7e37
F4101419 Henrique da Costa e Silva <henrique.costa@bb.com.br>
2021-07-02 15:23:08 -0300

    creates a project that saves a contacts info

---
+++ android/app/src/main/kotlin/br/com/example/bytebank/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.example.bytebank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,92 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatefulWidget {
+  @override
+  _ContactsListState createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        //initialData: [], //caso queiramos inserir um valor padrao antes do conteudo ser carregado lembrar de colocar tambem usando generics no FutureBuilder o tipo a ser esperado
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return null;
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
+              .then((value) => setState(() {}));
+        },
+        child: Icon(
+          Icons.add,
+          size: 40.0,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          '${contact.id.toString()} - ${contact.name}',
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.number.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+}
